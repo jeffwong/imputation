33c41441ccb71b6108a6f98e22d152a79bfe2339
jeffwong <jeff.ct.wong@gmail.com>
2013-01-13 13:51:55 -0500

    fix improper use of impute.prelim

--- R/meanImpute.R
+++ R/meanImpute.R
@@ -1,18 +1,20 @@
 #' Mean Imputation
+#'
 #' Fill missing values in a column with the mean of the column
 #' @param x a data frame or matrix where each row represents a different record
 #' @export
 meanImpute = function(x) {
-  prelim = impute.prelim(x)
+  prelim = impute.prelim(x, byrow=F)
   if (prelim$numMissing == 0) return (x)
   missing.matrix = prelim$missing.matrix
   x.missing = prelim$x.missing
 
-  x.imputed = apply(x, 2, function(j) {
+  x.imputed = apply(x.missing, 2, function(j) {
       bad.indices = which(is.na(j))
-      j[bad.indices] = mean(j, na.rm = T)
+      j[bad.indices] = mean(j[-1], na.rm = T)
       j
   })
+  x[,prelim$missing.cols.indices] = x.imputed
   
   return (list(x = x,
                missing.matrix = missing.matrix))
@@ -22,7 +24,7 @@ meanImpute = function(x) {
 #' CV for meanImpute
 #'
 #' Cross Validation for mean Imputation
-#' Artificially erase some data and run meanImpute compute the RMSE on 
+#' Artificially erase some data and run meanImpute to compute the RMSE on 
 #' the subset of x for which data was artificially erased.
 #' @param x a data frame or matrix where each row represents a different record
 #' @export
