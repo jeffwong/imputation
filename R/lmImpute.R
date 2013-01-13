cb94b4ba0c42d1ed3c8772db39fa9a8d35214202
jeffwong <jeff.ct.wong@gmail.com>
2013-01-13 13:49:42 -0500

    imputation using locally weighted regression

---
+++ R/lmImpute.R
@@ -0,0 +1,42 @@
+#' Locally Weighted Linear Imputation
+#'
+#' Fill missing values in a column by running a locally weighted least squares
+#' regression against the row number.
+#' Good for large data (large number of records)
+#' @param x a data frame or matrix where each row represents a different record
+#' @export
+lmImpute = function(x) {
+  prelim = impute.prelim(x, byrow=F)
+  if (prelim$numMissing == 0) return (x)
+  missing.matrix = prelim$missing.matrix
+  x.missing = prelim$x.missing
+
+  indices = 1:nrow(x)
+  x.imputed = apply(x.missing, 2, function(j) {
+    predict(locfit(j[-1] ~ indices), indices)
+  })
+  x[,prelim$missing.cols.indices] = x.imputed
+  
+  return (list(x = x,
+               missing.matrix = missing.matrix))
+
+}
+
+#' CV for lmImpute
+#'
+#' Cross Validation for Locally Weighted Linear Imputation
+#' Artificially erase some data and run lmImpute to compute the RMSE on 
+#' the subset of x for which data was artificially erased.
+#' @param x a data frame or matrix where each row represents a different record
+#' @export
+cv.lmImpute = function(x) {
+  prelim = cv.impute.prelim(x)
+  remove.indices = prelim$remove.indices
+  x.train = prelim$x.train
+
+  x.imputed = lmImpute(x.train)$x
+  error = (x[remove.indices] - x.imputed[remove.indices]) / x[remove.indices]
+  rmse = sqrt(mean(error^2))
+  
+  list(imputation = x.imputed, rmse = rmse)
+}
