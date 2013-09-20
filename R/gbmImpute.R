aed440bb2737bc295055390353f792cd5ad5bbde
jeffwong <jeff.ct.wong@gmail.com>
2013-09-20 12:17:59 -0700

    impute categorical vars by training gbm with multinomial distribution.  run predict to get a matrix of probabilities for the different factor levels.  sample the factor levels with these probabilities to get the imputed value

--- R/gbmImpute.R
+++ R/gbmImpute.R
@@ -19,55 +19,66 @@
 #'   x[x.missing] = NA
 #'   gbmImpute(x)
 #' @export
-gbmImpute = function(x, max.iters = 2, cv.fold = 2, n.trees = 100, verbose = T, ...) {
-  if (nrow(x) < 1000) warning("Tree based imputation works best with larger data (> 1000 obs)")
-  prelim = impute.prelim(x, byrow = F)
-  if (prelim$numMissing == 0) return (x)
-  missing.matrix = prelim$missing.matrix
-  missing.cols.indices = prelim$missing.cols.indices
- 
-  if (verbose) print(paste("Training over:", length(missing.cols.indices), "features"))
-  for (i in 1:max.iters) {
-    if (verbose) print (paste("Begin iteration: ", i))
-    x[,missing.cols.indices] = sapply(missing.cols.indices, function(j) {
-      if (verbose) print(paste("Imputing on feature: ", j))
-      #response variable in gbm cannot contain missing data
-      if (i == 1) good.data = which(!missing.matrix[,j])
-      else good.data = 1:nrow(x)
-      bad.data = which(missing.matrix[,j])
-      data = x[good.data,-j]
-      if (!is.data.frame(data)) data = data.frame(data = data)
-      gbm1 <- gbm(x[good.data,j] ~ .,
-                  data = data,
-                  var.monotone = rep(0, ncol(x)-1), # -1: monotone decrease,
-                  # +1: monotone increase,
-                  #  0: no monotone restrictions
-                  distribution="gaussian",     # bernoulli, adaboost, gaussian,
-                  # poisson, coxph, and quantile available
-                  n.trees=n.trees,                # number of trees
-                  shrinkage=0.005,             # shrinkage or learning rate,
-                  # 0.001 to 0.1 usually work
-                  interaction.depth=3,         # 1: additive model, 2: two-way interactions, etc.
-                  bag.fraction = 0.5,          # subsampling fraction, 0.5 is probably best
-                  train.fraction = 2/3,        # fraction of data for training,
-                  n.minobsinnode = 10,         # minimum total weight needed in each node
-                  cv.folds = cv.fold,                # do 5-fold cross-validation
-                  keep.data=TRUE,              # keep a copy of the dataset with the object
-                  verbose=verbose,
-                  ...)                # print out progress
-      best.iter <- gbm.perf(gbm1,method="test", plot.it = F)
-      newdata = x[bad.data,-j]
-      if (!is.data.frame(newdata)) newdata = data.frame(data = newdata)
-      data.predict = predict(gbm1, newdata = newdata, n.trees = best.iter)
-      x[bad.data,j] = data.predict
-      x[,j]
-    })
-  }
-  
-  return ( list (
-    x=x,
-    missing.matrix=missing.matrix
-  ))
+gbmImpute = function (x, max.iters = 2, cv.fold = 2, n.trees = 100, verbose = T,
+    ...)
+{
+    if (nrow(x) < 1000)
+        warning("Tree based imputation works best with larger data (> 1000 obs)")
+    prelim = impute.prelim(x, byrow = F, keep.x.missing=F)
+    if (prelim$numMissing == 0)
+        return(x)
+    missing.matrix = prelim$missing.matrix
+    missing.cols.indices = prelim$missing.cols.indices
+    if (verbose)
+        print(paste("Training over:", length(missing.cols.indices),
+            "missing features"))
+    for (i in 1:max.iters) {
+        if (verbose)
+            print(paste("Begin iteration: ", i))
+        x[, missing.cols.indices] = sapply(missing.cols.indices,
+            function(j) {
+                j = as.numeric(j)
+                if (verbose)
+                  print(paste("Imputing on feature: ", j))
+                if (i == 1)
+                  good.data = which(!missing.matrix[, j])
+                else good.data = 1:nrow(x)
+                bad.data = which(missing.matrix[, j])
+                data = x[good.data, -j]
+                if (!is.data.frame(data))
+                  data = data.frame(data = data)
+                datatype = data.class(x[good.data[1],j])
+                distribution = switch(datatype,
+                                      factor = "multinomial",
+                                      "gaussian")
+                y = x[good.data,j]
+                gbm1 <- gbm(y ~ ., data = data,
+                  var.monotone = rep(0, ncol(x) - 1), distribution = distribution,
+                  n.trees = n.trees, shrinkage = 0.005, interaction.depth = 3,
+                  bag.fraction = 0.5, train.fraction = 2/3, n.minobsinnode = 10,
+                  cv.folds = cv.fold, keep.data = TRUE, verbose = verbose,
+                  ...)
+                best.iter <- gbm.perf(gbm1, method = "test",
+                  plot.it = F)
+                newdata = x[bad.data, -j]
+                if (!is.data.frame(newdata))
+                  newdata = data.frame(data = newdata)
+                data.predict = predict(gbm1, newdata = newdata,
+                  n.trees = best.iter)
+                if(datatype != "factor")
+                  x[bad.data, j] = data.predict
+                if(datatype == "factor") {
+                  j.levels = levels(x[good.data[1],j])
+                  data.predict = as.data.frame(data.predict)
+                  multinomial.pred = data.predict = apply(data.predict,1,function(i) {
+                    sample(j.levels, prob = i - min(i), size = 1)
+                  })
+                  x[bad.data, j] = factor(multinomial.pred, levels = j.levels)
+                }
+                x[, j]
+            })
+    }
+    return(list(x = x, missing.matrix = missing.matrix))
 }
 
 #' CV for gbmImpute
@@ -90,7 +101,6 @@ cv.gbmImpute = function(x, ...) {
 
   x.imputed = gbmImpute(x.train, verbose=F, ...)$x
   error = (x[remove.indices] - x.imputed[remove.indices])
-  nerror = error / x[remove.indices]
   rmse = sqrt(mean(error^2))
   
   list(imputation = x.imputed, rmse = rmse)
